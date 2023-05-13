798c51db215791ce8962d7fe358735bd2a2d64f5
Eugen <evgen.bazhenov.2002@gmail.com>
2023-05-13 20:00:12 +0800

    add main template

--- bitrix/index.php
+++ bitrix/index.php
@@ -1,8 +1,8 @@
 <?
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
-?> 
-
+?>
+Hello world
 <?
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
 ?>
